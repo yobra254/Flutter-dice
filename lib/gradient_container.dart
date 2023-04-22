400260137315c51bd503972ea8924cbe284a64a8
yobra254 <brayodhis071@gmail.com>
2023-04-22 20:18:50 +0300

    Reusing widget Classes in positional arguments

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,24 +5,48 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 34, 4, 86),
-            Color.fromARGB(255, 11, 244, 162)
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyleText(),
+        child: StyleText('Hello World!!'),
       ),
     );
     //throw UnimplementedError();
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyleText('Hello World'),
+//       ),
+//     );
+//     //throw UnimplementedError();
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
 
+//Color.fromARGB(255, 34, 4, 86), Color.fromARGB(255, 11, 244, 162
+
 //defining a function
 void main() {
 //executing a function
@@ -8,7 +10,10 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         //backgroundColor: Color.fromARGB(255, 103, 44, 186),
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 34, 4, 86),
+          Color.fromARGB(255, 11, 244, 162),
+        ),
       ),
     ),
   );
